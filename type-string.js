de922f5a660a983cbc3a144b02948086e06f0fd5
Jefferson Matos <jefferson.contato@outlook.com.br>
2023-01-18 14:35:16 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Ola, mundo!";
+const text2 = 'Ola mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "35689";
+
+const citacao = "Meu nome é: ";
+const meuNome = "Jeff"
+
+//Concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template liberal
